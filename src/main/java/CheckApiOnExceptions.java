34150e1bd9d460d7e57db3a66ab24068c74800ec
oleg <legik7127@gmail.com>
2023-04-04 12:04:48 +0600

    added class Application and method run()

--- src/main/java/CheckApiOnExceptions.java
+++ src/main/java/CheckApiOnExceptions.java
@@ -1,13 +1,24 @@
 public class CheckApiOnExceptions {
+    public static void main(String[] args) {
+        Application.run();
+    }
 
     class MyArray{
 
     }
-    class MyArrayApi {
+    static class MyArrayApi {
         MyArray ma;
         public MyArrayApi(MyArray myArray) {
             ma = myArray;
-
+        }
+        public void build(){
+            Integer[] ar = {2, 10, 13, null, 7, null};
+        }
+    }
+    class Application{
+        static void run(){
+            MyArrayApi mapi = new MyArrayApi(null);
+            mapi.build();
         }
     }
 }
