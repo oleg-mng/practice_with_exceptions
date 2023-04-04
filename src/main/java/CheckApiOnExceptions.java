76a2fcbf8bc8cf0d243d6a4fffc46b49965a725e
oleg <legik7127@gmail.com>
2023-04-04 11:45:26 +0600

    added class CheckApiOnExceptions

---
+++ src/main/java/CheckApiOnExceptions.java
@@ -0,0 +1,13 @@
+public class CheckApiOnExceptions {
+
+    class MyArray{
+
+    }
+    class MyArrayApi {
+        MyArray ma;
+        public MyArrayApi(MyArray myArray) {
+            ma = myArray;
+
+        }
+    }
+}
