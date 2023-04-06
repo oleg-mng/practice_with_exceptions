7955504584c4e466d42071a2ca68285757bf6121
oleg <legik7127@gmail.com>
2023-04-06 09:22:59 +0600

    added class Format

--- src/main/java/AppSaveAs.java
+++ src/main/java/AppSaveAs.java
@@ -1,16 +1,54 @@
 import java.text.Format;
 
 public class AppSaveAs {
+    public Word (Format f){
+        if (f != null) format = f;
+        else fomat = DefaultFormat();
+    }
+
+    interface Format{
+        void Save(String path, String data);
+
+    }
+
+    class DocFormat {
+        @Override
+        public void Save(String path, String data) {
+            System.out.println("File save DocFormat..." + path + "data: \n" + data);
+        }
+    }
+    class TxtFormat {
+        @Override
+        public void Save(String path, String data) {
+            System.out.println("File save TxtFormat..." + path + "data: \n" + data);
+        }
+    }
+
+    class DocFormat implements Format {
+
+    }
+
+    class DocApp {
+        static void run
+
+        {
+            Word doc = new Word(null);
+            doc.append("line 1");
+            doc.SaveAs("Новый документ 2");
+            doc.setFormat(new DocFormat());
+            doc.append("line 7");
+        }
+    }
 
 
-    class DefaultFormat implements Format{
+    class DefaultFormat implements Format {
         Format format;
+
         public DefaultFormat();
 
         @Override
-        public void Save(String path, String data){
+        public void Save(String path, String data) {
             System.out.println("Формат не определен");
         }
-
     }
 }
