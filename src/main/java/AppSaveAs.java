d3364c7587c5d5b703047049a73237626333d3d8
oleg <legik7127@gmail.com>
2023-04-09 22:59:38 +0600

    the code is finalized and launched

--- src/main/java/AppSaveAs.java
+++ src/main/java/AppSaveAs.java
@@ -1,93 +1,91 @@
-import java.text.Format;
-
 public class AppSaveAs {
     public static void main(String[] args) {
         DocApp.run();
     }
+}
 
+class Word {
+    Format format;
+    StringBuilder sb = new StringBuilder();
 
-    class Word {
-        Format format;
-        StringBuilder sb = new StringBuilder();
-
-        public void setFormat(Format format) {
-            this.format = new Proxy(format);
-        }
-
-        public Word(Format f) {
-            setFormat(f);
-        }
-
-        public void append(String line) {
-            sb.append(line + "\n");
-        }
-
-        public void SaveAs(String path) {
-            format.Save(path, sb.toString());
-        }
+    public void setFormat(Format format) {
+        this.format = new Proxy(format);
     }
 
-    interface Format {
-        void Save(String path, String data);
+    public Word(Format f) {
+        setFormat(f);
     }
 
-    class DocFormat implements Format {
-        @Override
-        public void Save(String path, String data) {
-            System.out.println("File save DocFormat..." + path + "data: \n" + data);
-        }
+    public void append(String line) {
+        sb.append(line + "\n");
     }
 
-    class DocxFormat implements Format {
-        @Override
-        public void Save(String path, String data) {
-            System.out.println("File save DocxFormat..." + path + "data: \n" + data);
-        }
+    public void SaveAs(String path) {
+        format.Save(path, sb.toString());
     }
+}
 
-    class TxtFormat implements Format {
-        @Override
-        public void Save(String path, String data) {
-            System.out.println("File save TxtFormat..." + path + "data: \n" + data);
-        }
+interface Format {
+    void Save(String path, String data);
+}
+
+class DocFormat implements Format {
+    @Override
+    public void Save(String path, String data) {
+        System.out.println("File save DocFormat..." + path + "data: \n" + data);
     }
+}
 
-    class DocApp {
-        static void run() {
-            Word doc = new Word(null);
-            doc.append("line 1");
-            doc.SaveAs("Новый документ 2");
+class DocxFormat implements Format {
+    @Override
+    public void Save(String path, String data) {
+        System.out.println("File save DocxFormat..." + path + "data: \n" + data);
+    }
+}
+
+class TxtFormat implements Format {
+    @Override
+    public void Save(String path, String data) {
+        System.out.println("File save TxtFormat..." + path + "data: \n" + data);
+    }
+}
+
+class DocApp {
+    static void run() {
+        Word doc = new Word(null);
+        doc.append("line 1");
+        doc.SaveAs("Новый документ 2");
 //            doc.setFormat(null);
 //            doc.append("line 7");
 //            doc.SaveAs("Новый документ 3");
-        }
-    }
-
-    class DefaultFormat implements Format {
-        Format format;
-
-        public DefaultFormat() {
-        }
-
-        @Override
-        public void Save(String path, String data) {
-            System.out.println("Формат не определен");
-        }
-    }
-
-    class Proxy implements Format {
-        Format format;
-
-        public Proxy(Format f) {
-            if (f == null) format = new DefaultFormat();
-            else format = f;
-        }
-
-        @Override
-        public void Save(String path, String data) {
-            System.out.println("File Proxy..." + path + "data: \n" + data);
-        }
     }
 }
+
+class DefaultFormat implements Format {
+    Format format;
+
+    public DefaultFormat() {
+    }
+
+    @Override
+    public void Save(String path, String data) {
+        System.out.println("Формат не определен");
+    }
+}
+
+class Proxy implements Format {
+    Format format;
+
+    public Proxy(Format f) {
+        if (f == null) format = new DefaultFormat();
+        else format = f;
+    }
+
+    @Override
+    public void Save(String path, String data) {
+        System.out.println("File Proxy..." + path + "data: \n" + data);
+    }
+}
+
 
 
